bdd8fea6f546a75671977ecfc4b68856780e286e
KefirAbsorber <jan.bogdanowicz05@gmail.com>
2026-03-24 19:12:11 +0100

    Optymalizacja kodu, zmieniony zestaw testowy

--- warzywniak1.py
+++ warzywniak1.py
@@ -1,38 +1,63 @@
 from more_itertools import distinct_permutations
 
-zasady={('pomidor','ogorek'):10,('pomidor','zodkiewka'):-10,('zodkiewka','ogorek'):10}
+
+zasady_matrix =[ # stare x nowe
+#pom ogr rzod
+ [0,    10, -10 ], #pomidor
+ [-10,  -10,  10  ], #ogorek
+ [0,    10, 0   ]  #rzodkiewka
+]
+
+warzywa_mapping = {
+    "pomidor" : 0,
+    "ogorek" : 1,
+    "rzodkiewka" : 2,
+}
+
+warzywa_demapping = {v: k for k, v in warzywa_mapping.items()}
 
 def ewaluuj(stary_uklad, nowy_uklad):
     wartosc=0
     for i in range(len(stary_uklad)):
-        wartosc += zasady.get((stary_uklad[i], nowy_uklad[i]), 0)
+        wartosc += zasady_matrix[stary_uklad[i]][nowy_uklad[i]]
     return wartosc
-
-
 """
 warzywa = input("Podaj warzywa: ")
 warzywa=warzywa.split()
 """
-warzywa =  "ogorek zodkiewka pomidor pomidor pomidor pomidor pomidor ogorek"
+warzywa =  ("rzodkiewka rzodkiewka rzodkiewka rzodkiewka ogorek ogorek ogorek ogorek pomidor pomidor pomidor pomidor ")
+warzywa = warzywa.split()
+warzywa = [warzywa_mapping[x] for x in warzywa]
 uklady=distinct_permutations(warzywa)
 
 print("Permutacje done")
 
-stary_uklad='pomidor pomidor pomidor pomidor pomidor ogorek ogorek zodkiewka'
-stary_uklad=stary_uklad.split()
+stary_uklad = ( 'ogorek   ogorek     pomidor '
+                'ogorek   ogorek     pomidor '
+                'pomidor  rzodkiewka rzodkiewka '
+                'pomidor  rzodkiewka rzodkiewka')
+stary_uklad = stary_uklad.split()
+stary_uklad = [warzywa_mapping[x] for x in stary_uklad]
 
 
 maks=0
 najlepszy=[]
 for uklad in uklady:
-    uklad = list(uklad)
     aktualny=ewaluuj(stary_uklad, uklad)
     if aktualny>maks:
         maks=aktualny
-        najlepszy=uklad
+        najlepszy=[uklad]
     elif aktualny == maks:
         najlepszy.append(uklad)
 
-print(f"{najlepszy}:{maks}")
+opcje_demap=[]
+i=0
+for uklad in najlepszy:
+    opcje_demap.append([])
+    for t in uklad:
+        opcje_demap[i].append(warzywa_demapping.get(t))
+    i=i+1
 
-
+print("Najwyzsza wartosc: ", maks)
+for i in opcje_demap:
+    print(i)
